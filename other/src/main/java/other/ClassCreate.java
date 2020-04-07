79b8147c4bad66f96b4777e01d53d5b10f0b7380
huangyu <hy00870@163.com>
2020-04-08 07:09:54 +0800

    javassist

---
+++ other/src/main/java/other/ClassCreate.java
@@ -0,0 +1,11 @@
+package other;
+
+import javassist.ClassPool;
+
+public class ClassCreate {
+
+    private void create(){
+        ClassPool classPool = new ClassPool();
+//        Class.forName("")
+    }
+}
