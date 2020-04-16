e31e2de221e5b9e56650d33833019c3d93ad9af9
huangyu <hy00870@163.com>
2020-04-16 08:13:14 +0800

    mq transaction

--- transaction_common/src/main/java/com/hy/entity/Message.java
+++ transaction_common/src/main/java/com/hy/entity/Message.java
@@ -1,9 +1,12 @@
 package com.hy.entity;
 
+import java.math.BigDecimal;
+
 public class Message {
     private String id;
     private String ts;
     private String status;
     private String payUserid;
     private String receiveUserid;
+    private BigDecimal money;
 }
