3842d127eb426c9730c8db790647e0180c3f877b
huangyu <hy00870@163.com>
2020-04-17 06:12:55 +0800

    transaction pay

--- transaction_epay/src/main/java/com/hy/App.java
+++ transaction_epay/src/main/java/com/hy/App.java
@@ -1,4 +1,15 @@
 package com.hy;
 
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableRabbit
 public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(com.hy.App.class, args);
+    }
+
 }
---
+++ transaction_epay/src/main/java/com/hy/service/impl/YuebaoServiceImpl.java
@@ -0,0 +1,11 @@
+package com.hy.service.impl;
+
+import com.hy.service.YuebaoService;
+
+public class YuebaoServiceImpl implements YuebaoService {
+
+
+    public void doPay() {
+
+    }
+}
