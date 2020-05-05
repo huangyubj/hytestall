f136947ac325338ccb1c1576e2211b050c37ade7
huangyu <huangyu@ushayden.com>
2020-05-05 19:11:35 +0800

    flashsale

---
+++ falshsales/src/main/java/com/hy/config/ScriptConfig.java
@@ -0,0 +1,65 @@
+package com.hy.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.scripting.ScriptSource;
+import org.springframework.scripting.support.ResourceScriptSource;
+
+@Configuration
+public class ScriptConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(ScriptConfig.class);
+    @Bean
+    public RedisScript<Long> limitScript() {
+        RedisScript redisScript = null;
+        try {
+            ScriptSource scriptSource = new ResourceScriptSource(new ClassPathResource("/scripts/limit.lua"));
+//            log.info("script:{}", scriptSource.getScriptAsString());
+            redisScript = RedisScript.of(scriptSource.getScriptAsString(), Long.class);
+        } catch (Exception e) {
+            log.error("error", e);
+        }
+        return redisScript;
+
+    }
+
+    @Bean
+    public RedisScript<Boolean> lockScript() {
+        RedisScript<Boolean> redisScript = null;
+        try {
+            ScriptSource scriptSource = new ResourceScriptSource(new ClassPathResource("/scripts/lock.lua"));
+            redisScript = RedisScript.of(scriptSource.getScriptAsString(), Boolean.class);
+        } catch (Exception e) {
+            log.error("error" , e);
+        }
+        return redisScript;
+    }
+
+    @Bean
+    public RedisScript<Long> unlockScript() {
+        RedisScript<Long> redisScript = null;
+        try {
+            ScriptSource scriptSource = new ResourceScriptSource(new ClassPathResource("/scripts/unlock.lua"));
+            redisScript = RedisScript.of(scriptSource.getScriptAsString(), Long.class);
+        } catch (Exception e) {
+            log.error("error" , e);
+        }
+        return redisScript;
+    }
+
+    @Bean
+    public RedisScript<Long> rateLimitScript() {
+        RedisScript<Long> redisScript = null;
+        try {
+            ScriptSource scriptSource = new ResourceScriptSource(new ClassPathResource("/ratelimit/ratelimit.lua"));
+            redisScript = RedisScript.of(scriptSource.getScriptAsString(), Long.class);
+        } catch (Exception e) {
+            log.error("error" , e);
+        }
+        return redisScript;
+    }
+}
---
+++ falshsales/src/main/java/com/hy/service/impl/OrderServiceImpl.java
@@ -0,0 +1,40 @@
+package com.hy.service.impl;
+
+import com.hy.dao.CatalogMapper;
+import com.hy.dao.SalesOrderMapper;
+import com.hy.entity.Catalog;
+import com.hy.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(rollbackFor = Exception.class)
+@Service
+public class OrderServiceImpl implements OrderService {
+    private static final String CATALOG_TOTAL = "catalog_total";
+    private static final String CATALOG_SOLD = "catalog_sold";
+    @Autowired
+    private CatalogMapper catalogMapper;
+    @Autowired
+    private SalesOrderMapper salesOrderMapper;
+    @Autowired
+
+
+    RedisTemplate<String, String> redisTemplate;
+    @Override
+    public void initGoodsInfo() {
+        Catalog catalog = new Catalog();
+        catalog.setName("mac");
+        catalog.setTotal(1000);
+        catalog.setSold(0);
+        catalogMapper.insert(catalog);
+        redisTemplate.opsForValue().set(CATALOG_TOTAL + catalog.getId(), catalog.getTotal().toString());
+        redisTemplate.opsForValue().set(CATALOG_SOLD + catalog.getId(), catalog.getSold().toString());
+//        log.info("redis value:{}", redisTemplate.opsForValue().get(CATALOG_TOTAL + catalog.getId()));
+        handleCatalog();
+    }
+
+    private void handleCatalog() {
+    }
+}
